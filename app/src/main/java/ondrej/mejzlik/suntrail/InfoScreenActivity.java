1d4d7316f1292e0bb8c47d5a53803a2af5b335b1
athwale <mejzlik.ondrej@gmail.com>
2017-02-23 21:59:08 +0100

    Sun path basic information activity added

--- app/src/main/java/ondrej/mejzlik/suntrail/InfoScreenActivity.java
+++ app/src/main/java/ondrej/mejzlik/suntrail/InfoScreenActivity.java
@@ -1,8 +1,13 @@
 package ondrej.mejzlik.suntrail;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.webkit.WebView;
+import android.text.Html;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.TextView;
 
 public class InfoScreenActivity extends Activity {
 
@@ -10,8 +15,39 @@ public class InfoScreenActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info_screen_layout);
-        WebView webView = (WebView) findViewById(R.id.info_screen_web_view);
-        // Set the webview to have transparent background
-        webView.setBackgroundColor(0x00000000);
+
+        TextView textViewTop = (TextView) (findViewById(R.id.info_screen_text_view_top));
+        textViewTop.setText(getHtmlForTextView(getString(R.string.sun_path_info_top)));
+        textViewTop.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView textViewMiddle = (TextView) (findViewById(R.id.info_screen_text_view_middle));
+        textViewMiddle.setText(getHtmlForTextView(getString(R.string.sun_path_info_middle)));
+        textViewMiddle.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView textViewBottom = (TextView) (findViewById(R.id.info_screen_text_view_bottom));
+        textViewBottom.setText(getHtmlForTextView(getString(R.string.sun_path_info_bottom)));
+        textViewBottom.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    /**
+     * Converts string from html to a string displayable by TextView
+     * @param html Input string. Can be from values/strings if enclosed in <![CDATA[ ... ]]>
+     * @return Spanned string for TextView
+     */
+    // Removes warning for fromHtml which is deprecated in API 24
+    @SuppressWarnings("deprecation")
+    public static Spanned getHtmlForTextView(String html){
+        Spanned output;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            output = Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY);
+        } else {
+            output = Html.fromHtml(html);
+        }
+        return output;
+    }
+
+    public void mapButtonHandler(View view) {
+        Intent intent = new Intent(this, ZoomableWebViewActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/ondrej/mejzlik/suntrail/ZoomableWebViewActivity.java
@@ -0,0 +1,29 @@
+package ondrej.mejzlik.suntrail;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class ZoomableWebViewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zoomable_web_view);
+
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        WebView webView = (WebView) (findViewById(R.id.zoomable_webview));
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(false);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDisplayZoomControls(false);
+        webView.setBackgroundColor(Color.TRANSPARENT);
+
+        String data = "<body style='margin:0;padding:0;'>" + "<img src=\"overall_map_full_size.png\"/></body>";
+        webView.loadDataWithBaseURL("file:///android_asset/",data , "text/html", "utf-8",null);
+    }
+
+}
