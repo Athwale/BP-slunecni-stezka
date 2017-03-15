695435fb90eaf949cc1f6640a1d376de2113c2e4
athwale <mejzlik.ondrej@gmail.com>
2017-03-15 16:09:06 +0100

    Image optimizations and layout changes

--- app/src/main/java/ondrej/mejzlik/suntrail/activities/InfoScreenActivity.java
+++ app/src/main/java/ondrej/mejzlik/suntrail/activities/InfoScreenActivity.java
@@ -43,16 +43,14 @@ public class InfoScreenActivity extends Activity {
                 return;
             }
 
+            this.arguments = new Bundle();
             FragmentManager fragmentManager = getFragmentManager();
             FragmentTransaction transaction = fragmentManager.beginTransaction();
 
-            // Load the fragment according to which button we used
+            // Load fragment according to which button we used
             Intent intent = this.getIntent();
             int type = intent.getIntExtra(INFO_BUTTON_INTENT_KEY, INFO_BUTTON_GENERAL);
             if (type == INFO_BUTTON_GENERAL) {
-                // Prepare map for zoomable image argument
-                this.arguments = new Bundle();
-                arguments.putInt(IMAGE_KEY, R.drawable.overall_map_full_size);
                 SunPathInfoFragment infoFragment = new SunPathInfoFragment();
                 // Add the fragment to the fragment_container in FrameLayout
                 transaction.add(R.id.info_screen_fragment_container, infoFragment);
@@ -81,12 +79,23 @@ public class InfoScreenActivity extends Activity {
     }
 
     /**
-     * Handles clicks from display map button.
-     * Opens a new fragment with the map.
+     * Handles clicks from display map button and schema button.
+     * Opens a new zoomable image fragment with the image.
      *
      * @param view The button which has been clicked
      */
-    public void mapButtonHandler(View view) {
+    public void imageButtonsHandler(View view) {
+        switch (view.getId()) {
+            case R.id.sun_path_info_button_map: {
+                // Prepare map for zoomable image argument
+                this.arguments.putInt(IMAGE_KEY, R.drawable.pict_overall_map_full_size);
+                break;
+            }
+            case R.id.sun_path_info_button_schema: {
+                this.arguments.putInt(IMAGE_KEY, R.drawable.pict_schema_white);
+                break;
+            }
+        }
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
 
