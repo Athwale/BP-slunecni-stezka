8864f7e55167088ffd7f6e0b6efc6097d074a96c
athwale <mejzlik.ondrej@gmail.com>
2017-03-22 00:15:03 +0100

    Reading NFC Tag ID

--- app/src/main/java/ondrej/mejzlik/suntrail/activities/NfcScannerActivity.java
+++ app/src/main/java/ondrej/mejzlik/suntrail/activities/NfcScannerActivity.java
@@ -2,11 +2,13 @@ package ondrej.mejzlik.suntrail.activities;
 
 import android.animation.ObjectAnimator;
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.Intent;
-import android.nfc.NdefMessage;
+import android.content.IntentFilter;
 import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.NfcA;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
@@ -15,7 +17,14 @@ import android.widget.Toast;
 
 import ondrej.mejzlik.suntrail.R;
 
+/**
+ * MFC is not a dangerous permission, we do not have to ask for it.
+ */
 public class NfcScannerActivity extends Activity {
+    PendingIntent pendingIntent = null;
+    NfcAdapter nfcAdapter = null;
+    IntentFilter[] intentFiltersArray = null;
+    String[][] techListsArray = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,22 +53,120 @@ public class NfcScannerActivity extends Activity {
         animatorShip.setRepeatCount(ObjectAnimator.INFINITE);
         animatorShip.setInterpolator(new AccelerateDecelerateInterpolator());
         animatorShip.start();
+
+        this.handleNfc(getIntent());
+        this.setUpNfc();
+    }
+
+    private void setUpNfc() {
+        // Create nfc adapter to read the tag
+        this.nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (this.nfcAdapter == null) {
+            // Device does not support NFC
+            Toast.makeText(this, getString(R.string.nfc_scanner_nfc_not_supported), Toast.LENGTH_LONG).show();
+        } else if (!this.nfcAdapter.isEnabled()) {
+            // NFC is disabled
+            Toast.makeText(this, getString(R.string.nfc_scanner_nfc_disabled), Toast.LENGTH_LONG).show();
+        } else {
+            // Android will save tag data into this intent
+            this.pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        }
+
+        IntentFilter ndef = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        try {
+            ndef.addDataType("*/*");
+        } catch (IntentFilter.MalformedMimeTypeException e) {
+            throw new RuntimeException("fail", e);
+        }
+        this.intentFiltersArray = new IntentFilter[]{ndef,};
+        this.techListsArray = new String[][]{new String[]{NfcA.class.getName()}};
     }
 
     @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-        Toast.makeText(this, "saved", Toast.LENGTH_SHORT).show();
-        if (intent != null && NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
-            Parcelable[] rawMessages =
-                    intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-            if (rawMessages != null) {
-                NdefMessage[] messages = new NdefMessage[rawMessages.length];
-                for (int i = 0; i < rawMessages.length; i++) {
-                    messages[i] = (NdefMessage) rawMessages[i];
-                }
-                // Process the messages array.
+    public void onPause() {
+        super.onPause();
+        this.nfcAdapter.disableForegroundDispatch(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        try {
+            // If the user disabled nfc then onResume can not access the nfcAdapter.
+            // We close the activity and setUpNfc shows a toast.
+            this.nfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFiltersArray, techListsArray);
+        } catch (NullPointerException ex) {
+            this.finish();
+        }
+        // If NFC is disabled while the nfc scanner activity is running and the user returns to it.
+        if (!this.nfcAdapter.isEnabled()) {
+            Toast.makeText(this, getString(R.string.nfc_scanner_nfc_disabled), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        // We call this method from onCreate too so processing has to be done elswhere.
+        this.handleNfc(intent);
+    }
+
+    private void handleNfc(Intent intent) {
+        String tagId = this.getTagId(intent);
+        Toast.makeText(this, tagId, Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * This method returns the TAG ID
+     *
+     * @param intent Intent from onCreate or onNewIntent
+     * @return Tag ID
+     */
+    private String getTagId(Intent intent) {
+        String id = "";
+        if (intent != null && NfcAdapter.ACTION_TECH_DISCOVERED.equals(intent.getAction())) {
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            // Get tag ID
+            byte[] tagId = tag.getId();
+            for (byte aTagId : tagId) {
+                // Bytes in android are signed, we need unsigned bytes and convert them to string.
+                id += Integer.toHexString(aTagId & 0xFF);
             }
         }
+        return id;
+    }
+
+    /**
+     * This method gets all tag info from the NFC tag. It is used in hidden developer mode.
+     *
+     * @param intent Intent from onCreate or onNewIntent
+     */
+
+    private String getTagInfo(Intent intent) {
+        String id = "No ID";
+        if (intent != null && NfcAdapter.ACTION_TECH_DISCOVERED.equals(intent.getAction())) {
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            // Read tag info
+            // General info
+            String tagInfo = tag.toString() + "\n";
+            // ID
+            tagInfo += "Tag ID: ";
+            byte[] tagId = tag.getId();
+            id = "";
+            tagInfo += "Length= " + tagId.length + "\n" + "ID: ";
+            for (byte aTagId : tagId) {
+                // Bytes in android are signed, we need unsigned bytes and convert them to string.
+                id += Integer.toHexString(aTagId & 0xFF);
+            }
+            tagInfo += id;
+            // Tech contents
+            String[] techList = tag.getTechList();
+            tagInfo += "\nTech List ";
+            tagInfo += "length = " + techList.length + "\n";
+            for (String aTechList : techList) {
+                tagInfo += aTechList + "\n ";
+            }
+            Toast.makeText(this, tagInfo, Toast.LENGTH_LONG).show();
+        }
+        return id;
     }
 }
