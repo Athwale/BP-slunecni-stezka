aa7bb0b9de164785db87b6747f43cb239a1a379a
athwale <mejzlik.ondrej@gmail.com>
2017-03-21 18:06:00 +0100

    NFC intent

--- app/src/main/java/ondrej/mejzlik/suntrail/activities/NfcScannerActivity.java
+++ app/src/main/java/ondrej/mejzlik/suntrail/activities/NfcScannerActivity.java
@@ -2,11 +2,16 @@ package ondrej.mejzlik.suntrail.activities;
 
 import android.animation.ObjectAnimator;
 import android.app.Activity;
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import ondrej.mejzlik.suntrail.R;
 
@@ -39,5 +44,22 @@ public class NfcScannerActivity extends Activity {
         animatorShip.setRepeatCount(ObjectAnimator.INFINITE);
         animatorShip.setInterpolator(new AccelerateDecelerateInterpolator());
         animatorShip.start();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Toast.makeText(this, "saved", Toast.LENGTH_SHORT).show();
+        if (intent != null && NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
+            Parcelable[] rawMessages =
+                    intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if (rawMessages != null) {
+                NdefMessage[] messages = new NdefMessage[rawMessages.length];
+                for (int i = 0; i < rawMessages.length; i++) {
+                    messages[i] = (NdefMessage) rawMessages[i];
+                }
+                // Process the messages array.
+            }
+        }
     }
 }
