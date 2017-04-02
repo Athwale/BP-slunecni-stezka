9ec0860d4f91d0bccfe57158b27c833685780df8
athwale <mejzlik.ondrej@gmail.com>
2017-04-02 12:18:39 +0200

    Improved <p> handling

--- app/src/main/java/ondrej/mejzlik/suntrail/utilities/HtmlConverter.java
+++ app/src/main/java/ondrej/mejzlik/suntrail/utilities/HtmlConverter.java
@@ -18,13 +18,33 @@ public class HtmlConverter {
      */
     // Removes warning for fromHtml which is deprecated in API 24
     @SuppressWarnings("deprecation")
-    public Spanned getHtmlForTextView(String html) {
+    public CharSequence getHtmlForTextView(String html) {
         Spanned output;
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
             output = Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY);
         } else {
             output = Html.fromHtml(html);
         }
-        return output;
+        return this.trim(output);
+    }
+
+    /**
+     * This method removes any white characters from the beginning and then from the end of the
+     * passed char sequence. Because html conversion always puts two \n below a <p> tag which
+     * we do not want there.
+     *
+     * @param text input text from Html.fromHtml()
+     * @return trimmed text
+     */
+    private CharSequence trim(CharSequence text) {
+        int start = 0;
+        int end = text.length();
+        while (start < end && Character.isWhitespace(text.charAt(start))) {
+            start++;
+        }
+        while (end > start && Character.isWhitespace(text.charAt(end - 1))) {
+            end--;
+        }
+        return text.subSequence(start, end);
     }
 }
