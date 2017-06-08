4dc68654e495a009fdee9f315bbbf5e6e0576454
athwale <mejzlik.ondrej@gmail.com>
2017-06-08 16:14:29 +0200

    Last reward added Game can now end on Saturn

--- app/src/main/java/ondrej/mejzlik/suntrail/fragments/GameMenuFragment.java
+++ app/src/main/java/ondrej/mejzlik/suntrail/fragments/GameMenuFragment.java
@@ -7,10 +7,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import ondrej.mejzlik.suntrail.R;
 
+import static ondrej.mejzlik.suntrail.utilities.PlanetIdentifier.PLANET_ID_SATURN;
 import static ondrej.mejzlik.suntrail.utilities.PlanetResourceCollector.PLANET_ID_KEY;
 import static ondrej.mejzlik.suntrail.utilities.PlanetResourceCollector.PLANET_NAME_KEY;
 import static ondrej.mejzlik.suntrail.utilities.PlanetResourceCollector.PLANET_QUARTER_PHOTO_KEY;
@@ -48,12 +50,22 @@ public class GameMenuFragment extends Fragment {
 
         TextView title = (TextView) (view.findViewById(R.id.game_menu_title));
         ImageView planetQuarterPhoto = (ImageView) (view.findViewById(R.id.game_menu_image_view_photo));
+        LinearLayout endGameLayout = (LinearLayout) (view.findViewById(R.id.game_menu_linear_layout_exit));
 
         // Resources must contain planet id
         if (resources != null && resources.containsKey(PLANET_ID_KEY)) {
             planetQuarterPhoto.setImageResource(resources.getInt(PLANET_QUARTER_PHOTO_KEY));
             String newTitle = title.getText() + " " + getResources().getString(resources.getInt(PLANET_NAME_KEY));
             title.setText(newTitle);
+
+            // The trip can end early at the Saturn board
+            if (resources.getInt(PLANET_ID_KEY) == PLANET_ID_SATURN) {
+                // Enable end game button
+                endGameLayout.setVisibility(View.VISIBLE);
+            } else {
+                // Disable end game button
+                endGameLayout.setVisibility(View.GONE);
+            }
         }
     }
 }
